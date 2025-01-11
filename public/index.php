c3b3fb6479defd343ed3556afd1905346e11ea9e
Hasibur Rahman <hasib.webdev@gmail.com>
2025-01-11 07:09:54 +0600

    Composer init

--- public/index.php
+++ public/index.php
@@ -2,12 +2,6 @@
 const BASE_PATH = __DIR__ . "/../";
 
 require BASE_PATH . 'utils/functions.php';
-
-spl_autoload_register(function ($class): void {
-    // Core\Database
-    $result = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$result}.php");
-});
+require base_path("vendor/autoload.php");
 
 require "./bootstrap.php";

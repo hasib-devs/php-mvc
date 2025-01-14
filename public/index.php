971c5a5b6a497cdd401f1cc0201cbeca15793fd1
Hasibur Rahman <hasib.webdev@gmail.com>
2025-01-14 09:18:59 +0600

    Add bootstrap file and update composer dependencies

--- public/index.php
+++ public/index.php
@@ -3,5 +3,4 @@ const BASE_PATH = __DIR__ . "/../";
 
 require BASE_PATH . 'utils/functions.php';
 require base_path("vendor/autoload.php");
-
-require "./bootstrap.php";
+require base_path("bootstrap.php");
